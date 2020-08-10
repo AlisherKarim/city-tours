dabf60d2530a36a29df20da0d36b7a6f5c223941
Alisher Karim <alisherktlwik@gmail.com>
2020-08-10 15:05:36 +0500

    first git commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Navbar from "./components/navbar/Navbar"
+import TourList from "./components/tourlist"
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar/>
+      <TourList/>
+    </React.Fragment>
+  );
+}
+
+export default App;
